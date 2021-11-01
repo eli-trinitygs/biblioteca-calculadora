fbf8f01326fb45b59ef678ef7c4a188c945c117c
Eli <elizabethtrinitygs@gmail.com>
2021-11-01 15:50:55 -0600

    creando boton en htm para calcular perimetro y area del cuadrado

--- figuras.js
+++ figuras.js
@@ -35,7 +35,7 @@ function perimetroTriangulo(lado1, lado2, base) {
 function areaTriangulo(base,altura) {
     return (base * altura) /2;
  }
-// console.log('El área del triángulo es: ' + areaTriangulo + 'cms2');
+console.log('El área del triángulo es: ' + areaTriangulo + 'cm2');
 
 console.groupEnd();
 
@@ -67,5 +67,20 @@ function areaCirculo(radio) {
 // console.log('El área del círculo es: ' + areaCirculo + 'cms2');
 
 console.groupEnd();
+
+//JS para el HTML
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById('InputCuadrado');
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+};
+
+function calcularAreaCuadrado() {
+    const input = document.getElementById('InputCuadrado');
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}
 
 
